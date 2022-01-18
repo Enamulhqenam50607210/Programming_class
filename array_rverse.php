b3158a938dd59834d99e2ea0f0e71654c738e144
@Enamulhqenam50607210 <enamulhqenam50607210@gmail.com>
2022-01-19 03:14:15 +0600

    first commit

---
+++ array_rverse.php
@@ -0,0 +1,15 @@
+<?php
+$names = ['Enamul_hq','Ferdawus','Jahid','Shomiran','Jamal','Emon'];
+$size = sizeof($names);
+$sub_array1 =[];
+$sub_array2 =[];
+$new_array =[];
+// ai loop use kore array k 2 vage vag korlam ai jonno size/2 dilam.. j hetu array index mene chole tia (-1) dolam
+for ($i = $size/2-1; $i >=0 ; $i--){
+    array_push($sub_array1,$names[$i]);
+}
+for ($j = $size -1 ; $j >=$size/2; $j--){
+    array_push($sub_array2,$names[$j]);
+}
+var_dump($sub_array1);
+// var_dump($sub_array2);
